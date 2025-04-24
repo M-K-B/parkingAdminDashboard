16f73b5e6954a0295cb98228a6520e84fda866de
M-K-B <m.bradshaw009@outlook.com>
2025-04-24 04:05:54 +0100

    Trigger deploy after permissions fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: "/parkingAdminDashboard/", // 👈 required for GitHub Pages
+  base: "//parkingAdminDashboard", // 👈 set to match your GitHub repo name
   plugins: [react()],
 });
